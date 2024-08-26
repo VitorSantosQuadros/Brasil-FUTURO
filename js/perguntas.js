d49e3e53bbbdba9cabcf5dae54e068ab35dd6dab
Vitor Quadros <107114183+VitorSantosQuadros@users.noreply.github.com>
2024-08-26 11:36:22 -0300

    Update perguntas.js

---
+++ js/perguntas.js
@@ -0,0 +1,169 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com a corrupção presente no nosso país. Qual a sua primeira impressão?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "No início ficou com medo dessa realidade.",
+                    "Ficou em pânico com tudo isso.",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                    "Quis saber como se juntava aos corruptos safados.",
+                    "Você só se entregou ao crime mesmo...",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta corrupção desemfreada, chamada politica, no fim vem um pensamento na sua mente que talvez pudessemos reverter essa situação. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Começa a pesquisar mais sobre o assunto, e busca conhecer pessoas boas com ideias incriveis sobre isso.",
+                afirmacao: [
+                    "Conseguiu utilizar a internet e o network para buscar informações úteis.",
+                    "Começa a utilizar os livros para conseguir a sabedoria e novas ideias sobre o mundo.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Preferiu a continuar a viver a sua vida como se nada estivesse acontecendo.",
+                afirmacao: [
+                    "Sentiu mais facilidade em continuar na sua ignorância kkkkkkkkk.",
+                    "Você preferiu ficar na sua sem pesquisar nada.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Após o conhecimento adquirido por meio de pesquisas, você começa a ser conhecido pela população brasileira. Com essa noticia, como você se sente?",
+        alternativas: [
+            {
+                texto: "Fico empolgado por meu trabalho esta sendo reconhecido pelas pessoas.",
+                afirmacao: [
+                    "Com bastante gente gostando dos meus conhecimentos que adquiri, isso vem me motivando ainda mais.",
+                    "Percebeu que se continuar se esforçando você irá ser conhecido mundialmente.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Fico timido de que a maioria da população sabe o que eu estou fazendo.",
+                afirmacao: [
+                    "Mesmo sua preocupação com as pessoas de saberem do seu trabalho você encontra forças para continuar.",
+                    "O espirito da bravura entrou em você e a timidez logo passou.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final de todos os estudos e popularidade que você adquiriu, você acaba se tornando presidente do Brasil. E agora?",
+        alternativas: [
+            {
+                texto: "Com uma estratégia bem elaborada você faz as melhores escolhas possiveis, e o Brasil acaba finalmente se tornando uma potencia mundial.",
+                afirmacao: [
+                    "A população te ama, e você se sente realizado por ter ajudado tantas pessoas de uma só vez.",
+                    "Todos te amam, você se sente o héroi deles."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Com o poder que você ganhou, você começa a se corromper e acaba tornando o Brasil uma potencia mundial, mas também uma monarquia.",
+                afirmacao: [
+                    "A população se sente traida e agradecida ao mesmo tempo, e você ganha tanto poder mas se sente tão triste e deprimido por ter no fundo traido o povo.",
+                    "Você acaba se tornando o vilão da população brasileira."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
